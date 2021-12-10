7cea29e573be10c207232294919022c87a8f0efb
Pete Barrow <pete@vega.xyz>
2021-12-10 13:19:35 +0000

    Fixed cut and paste error

--- cmd/snapshotdb.go
+++ cmd/snapshotdb.go
@@ -22,7 +22,7 @@ func init() {
 	rootCmd.AddCommand(snapshotDBCmd)
 	snapshotDBCmd.Flags().StringVarP(&snapshotDBOpts.databasePath, "db-path", "d", "", "path to the goleveldb database folder")
 	snapshotDBCmd.Flags().BoolVarP(&snapshotDBOpts.versionCountOnly, "versions", "v", false, "display the number of stored versions")
-	streamCmd.MarkFlagRequired("db-path")
+	snapshotDBCmd.MarkFlagRequired("db-path")
 }
 
 func runSnapshotDBCmd(cmd *cobra.Command, args []string) error {
