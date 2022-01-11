1f91c6a4129e8f4fd9dba95e43ffa923c368e930
wwestgarth <william@vega.xyz>
2022-01-11 16:14:49 +0000

    output from vegatools snapshotdb is not valid JSON

--- snapshotdb/snapshotdb.go
+++ snapshotdb/snapshotdb.go
@@ -1,12 +1,60 @@
 package snapshotdb
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/cosmos/iavl"
 	"github.com/syndtr/goleveldb/leveldb/opt"
 	db "github.com/tendermint/tm-db"
 )
+
+type SnapshotData struct {
+	Version int64 `json:"version"`
+	Height  int64 `json:"height"`
+	Size    int64 `json:"size"`
+}
+
+func displaySnapshotData(tree *iavl.MutableTree, versions []int) error {
+	j := struct {
+		Snapshots []SnapshotData `json:"snapshots"`
+	}{}
+
+	for _, version := range versions {
+
+		v, err := tree.LazyLoadVersion(int64(version))
+		if err != nil {
+			return err
+		}
+		j.Snapshots = append(j.Snapshots, SnapshotData{
+			Version: v,
+			Height:  int64(tree.Height()),
+			Size:    tree.Size(),
+		})
+	}
+
+	b, err := json.Marshal(j)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(b))
+	return nil
+}
+
+func displayNumberOfVersions(versions int) error {
+	j := struct {
+		Versions int64 `json:"n_versions"`
+	}{
+		Versions: int64(versions),
+	}
+
+	b, err := json.Marshal(j)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(b))
+	return nil
+}
 
 // Run is the main entry point for this tool
 func Run(dbpath string, versionsOnly bool) error {
@@ -21,22 +69,17 @@ func Run(dbpath string, versionsOnly bool) error {
 	}
 
 	tree, err := iavl.NewMutableTree(db, 0)
-	_, err = tree.Load()
+	if err != nil {
+		return err
+	}
+
+	if _, err := tree.Load(); err != nil {
+		return err
+	}
 	versions := tree.AvailableVersions()
 
 	if versionsOnly {
-		// Only output a json version of the number of snapshot versions that are in this database
-		fmt.Printf("{ Versions: %d }\n", len(versions))
-	} else {
-		// Run through all the snapshots and display some details
-		fmt.Printf("{ Snapshots: {\n")
-		for _, version := range versions {
-			v, err := tree.LazyLoadVersion(int64(version))
-			if err == nil {
-				fmt.Printf("  { Version: %d, Height: %d, Size: %d },\n", v, tree.Height(), tree.Size())
-			}
-		}
-		fmt.Printf(" }\n}\n")
+		return displayNumberOfVersions(len(versions))
 	}
-	return nil
+	return displaySnapshotData(tree, versions)
 }
