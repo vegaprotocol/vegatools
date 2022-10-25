a3c7a2594f15a4198e7a96af026b6a9ae4af65c6
Peter Barrow <pete@vega.xyz>
2022-10-25 15:42:58 +0100

    feat: Linter issues

--- perftest/batchorders.go
+++ perftest/batchorders.go
@@ -2,16 +2,19 @@ package perftest
 
 import commandspb "code.vegaprotocol.io/vega/protos/vega/commands/v1"
 
+// BatchOrders stores pending messages ready to be sent in a batch
 type BatchOrders struct {
 	cancels []*commandspb.OrderCancellation
 	amends  []*commandspb.OrderAmendment
 	orders  []*commandspb.OrderSubmission
 }
 
+// GetMessageCount returns the number of meesages waiting to be sent for this one user
 func (b *BatchOrders) GetMessageCount() int {
 	return len(b.cancels) + len(b.amends) + len(b.orders)
 }
 
+// Empty clears any pending messages
 func (b *BatchOrders) Empty() {
 	b.cancels = b.cancels[:0]
 	b.amends = b.amends[:0]
